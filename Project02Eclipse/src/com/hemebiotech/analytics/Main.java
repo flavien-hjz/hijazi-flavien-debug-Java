10b09f9aa6a47806f4e72a8a00de58f647270853
Flavien Hijazi <flavien.hijazi@gmail.com>
2023-07-31 10:06:38 +0200

    main implementation

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,21 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String filepath = "symptoms.txt";
+		ISymptomReader reader = new ReadSymptomDataFromFile(filepath);
+		ISymptomWriter writer = new WriteSymptomDataToFile();
+		AnalyticsCounter counter = new AnalyticsCounter(reader,writer);
+		List<String> symptomsList = counter.getSymptoms();
+		Map<String,Integer> symptomsMap = counter.countSymptoms(symptomsList);
+		Map<String,Integer> symptomsSorted = counter.sortSymptoms(symptomsMap);
+		counter.writeSymptoms(symptomsSorted);
+
+	}
+
+}
